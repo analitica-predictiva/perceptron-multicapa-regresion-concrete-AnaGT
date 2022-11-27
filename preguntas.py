8f44939c1677c1748df15c008e6f7d0ee61063a9
AnaGT <86501883+AnaGT@users.noreply.github.com>
2022-11-27 18:13:23 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -83,14 +83,7 @@ def pregunta_03():
             ),
             (
                 "mlpregressor",
-                MLPRegressor(
-                hidden_layer_sizes=(8,),
-                activation="relu",
-                learning_rate="adaptive",
-                momentum=0.9,
-                learning_rate_init=0.01,
-                max_iter=5000,
-            )  
+                MLPRegressor()  
             ),
         ],
     )
@@ -119,7 +112,7 @@ def pregunta_04():
 
     param_grid = {
         'hidden_layer_sizes':[1,8],  
-        'activation':['relu'],  
+        #'activation':['relu'],  
         'learning_rate':['adaptive'],  
         'momentum':[0.7,0.8,0.9],  
         'learning_rate_init':[0.01, 0.05, 0.1],  
