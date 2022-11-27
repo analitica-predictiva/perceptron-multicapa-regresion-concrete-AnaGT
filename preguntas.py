9a4b46338f4dbf25e0b6881c50d3fc5937c3d25e
AnaGT <86501883+AnaGT@users.noreply.github.com>
2022-11-26 19:08:16 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -111,13 +111,13 @@ def pregunta_04():
     #   * Use parada temprana
 
     param_grid = {
-        hidden_layer_sizes:(8,),  
-        activation: 'relu',  
-        learning_rate:'adaptive',  
-        momentum:[0.7,0.8,0.9],  
-        learning_rate_init:[0.01, 0.05, 0.1],  
-        max_iter:5000,  
-        early_stopping:True,  
+        'hidden_layer_sizes':[(8,)],  
+        'activation':['relu'],  
+        'learning_rate':['adaptive'],  
+        'momentum':[0.7,0.8,0.9],  
+        'learning_rate_init':[0.01, 0.05, 0.1],  
+        'max_iter':[5000],  
+        'early_stopping':[True],  
     }
 
     estimator = pregunta_03()
